8c85be26c9bd7536ced81680a1fa0f35e1d7f50a
Julian O'Brien <jmobrien@luminapower.com>
2010-10-27 22:05:31 -0400

    show only current auctions for player

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,10 @@ class Player < ActiveRecord::Base
   has_many :auctions,
     :order => 'last_seen_scan_id DESC, (buyout / quantity) DESC, quantity DESC',
     :include => [:item, :last_seen_scan, :first_seen_scan]
+
+  has_many :current_auctions,
+    :class_name => 'Auction',
+    :conditions => ['last_seen_scan_id = ?', Scan.latest_scan_id],
+    :order => 'last_seen_scan_id DESC, (buyout / quantity) DESC, quantity DESC',
+    :include => [:item, :last_seen_scan, :first_seen_scan]
 end
